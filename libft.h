969ac510d518ee8accab3cbf87f5cb5bdba11980
João Pedro <joaopedrorodrigues1333@gmail.com>
2026-01-12 19:44:26 -0300

    Add linked list functions to libft.h

--- libft.h
+++ libft.h
@@ -15,29 +15,41 @@
 
 # include <stdlib.h>
 
+typedef struct	s_list
+{
+	void			*content;
+	struct s_list	*next;
+}				t_list;
+
 void		ft_bzero(void *s, size_t n);
+void	ft_lstadd_back(t_list **lst, t_list *new);
+void	ft_lstadd_front(t_list **lst, t_list *new);
+void	ft_lstdelone(t_list *lst, void (*del)(void *));
+void		*ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memmove(void *dest, const void *src, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
+void		*ft_memchr(const void *s, int c, size_t n);
+char		*ft_strchr(const char *str, int c);
+char		*ft_strdup(const char *s);
+char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s1, char const *set);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+char		*ft_strrchr(const char *s, int c);
 int			ft_isalnum(int c);
 int			ft_isalpha(int c);
 int			ft_isascii(int c);
 int			ft_isdigit(int c);
 int			ft_isprint(int c);
-void		*ft_memcpy(void *dest, const void *src, size_t n);
-void		*ft_memmove(void *dest, const void *src, size_t n);
-void		*ft_memset(void *s, int c, size_t n);
-char		*ft_strchr(const char *str, int c);
-char		*ft_strdup(const char *s);
-char		*ft_strjoin(char const *s1, char const *s2);
-size_t		ft_strlcpy(char *dst, const char *src, size_t size);
-size_t		ft_strlen(const char *s);
-char		*ft_strtrim(char const *s1, char const *set);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
 int			ft_tolower(int c);
 int			ft_touper(int c);
-char		*ft_strrchr(const char *s, int c);
 int			ft_atoi(const char *nptr);
 int			ft_strncmp(char const *s1, char const *s2, size_t n);
-void		*ft_memchr(const void *s, int c, size_t n);
 int			ft_memcmp(const void *s1, const void *s2, size_t n);
-char		*ft_strmapi(char const *s, char (*f)(unsigned int, char));
+int		ft_lstsize(t_list *lst);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+size_t		ft_strlen(const char *s);
+t_list	*ft_lstnew(void *content);
+t_list	*ft_lstlast(t_list *lst);
 
 #endif
